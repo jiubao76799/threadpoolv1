3c327532d76888c94d848ea60a9f5b161c7db0e5
jiubao76799 <jiubao76799@gmail.com>
2026-02-13 07:00:17 -0800

    远程day1已完成

---
+++ build/test/CMakeFiles/test_day1_basic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_day1_basic.dir/test1.cpp.o"
+  "test_day1_basic"
+  "test_day1_basic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_day1_basic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
