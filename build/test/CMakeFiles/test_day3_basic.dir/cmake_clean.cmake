56da3bff84cdd321debe97c0c86096c7c502b20e
jiubao76799 <jiubao76799@gmail.com>
2026-02-22 07:27:54 -0800

    day3已完成

---
+++ build/test/CMakeFiles/test_day3_basic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_day3_basic.dir/test3.cpp.o"
+  "test_day3_basic"
+  "test_day3_basic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_day3_basic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- include/ThreadPool.h
+++ include/ThreadPool.h
@@ -24,6 +24,15 @@ public:
     //析构函数
     ~ThreadPool();
 
+    //提交任务到线程池
+    template<class F, class... Args>
+    auto enqueue(F&& f, Args&&... args)
+        -> std::future<typename std::result_of<F(Args...)>::type>;
+        
+
+    //状态查询方法
+    bool isStopped() const { return stop; }
+    
 private:
     //工作线程函数
     void workerThread();
@@ -43,6 +52,35 @@ private:
 
 };
 
+//模板函数实现
+template<class F, class... Args>
+auto ThreadPool::enqueue(F&& f,Args&&... args)
+    ->std::future<typename std::result_of<F(Args...)>::type>{
+
+        using return_type= typename std::result_of<F(Args...)>::type;
+        
+        //创建任务包装
+        auto task = std::make_shared<std::packaged_task<return_type()>>(
+            std::bind(std::forward<F>(f),std::forward<Args>(args)...)
+        );
+
+       std::future<return_type> result = task->get_future();
+
+       {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+
+            //线程池已停止，不能添加任务，抛出异常
+            if(stop){
+                throw std::runtime_error("enqueue on stopped ThreadPool");
+            }
+
+            //添加任务到队列
+            tasks.emplace([task](){ (*task)(); });
+       }
+
+       condition.notify_one(); //通知一个工作线程有新任务
+       return result;
+    }
 
 
 
